d8d4163500389bbd3e7dd2ee1aec8321823fc955
Arko McCallister <arkomccallister@Arkos-MacBook-Pro.local>
2024-10-24 23:18:16 +0600

    third commit local stroage

--- output/script.js
+++ output/script.js
@@ -4,3 +4,12 @@ const taskList = document.getElementById('taskList');
 const totalDisplay = document.getElementById('total');
 const completedDisplay = document.getElementById('completed');
 const pendingDisplay = document.getElementById('pending');
+
+// learned from brocode local stroage
+
+function loadTasks() {
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+        tasks = JSON.parse(storedTasks);
+    }
+}
