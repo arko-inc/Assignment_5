28feb7001b573d166aae5646d3e13aaa7010433b
Arko McCallister <arkomccallister@Arkos-MacBook-Pro.local>
2024-10-24 23:15:57 +0600

    second commit

---
+++ output/script.js
@@ -0,0 +1,6 @@
+let tasks = [];
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById('taskList');
+const totalDisplay = document.getElementById('total');
+const completedDisplay = document.getElementById('completed');
+const pendingDisplay = document.getElementById('pending');
